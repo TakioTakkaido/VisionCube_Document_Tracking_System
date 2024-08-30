c60dbc2c0ac3a88e82b61e5f3b639778cbd01e98
Takio Takkaido <papajoshua100804@gmail.com>
2024-08-31 03:05:39 +0800

    Added user roles as enum.

---
+++ COE_DocuTrackSys/app/Role.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+// VISION CUBE SOFTWARE CO. 
+// Enum: Roles
+// Contains the different users of the system.
+// Contributor/s: 
+// Calulut, Joshua Miguel C.
+
+enum Role: string
+{
+    case ADMIN = 'admin';
+    case USER = 'user';
+}
