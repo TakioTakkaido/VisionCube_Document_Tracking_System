4209230f3a4ddd715aa8377fe9cb1307bd905598
Takio Takkaido <papajoshua100804@gmail.com>
2024-08-31 03:58:06 +0800

    Added middleware for role authentication

---
+++ COE_DocuTrackSys/app/Http/Middleware/UserRoleMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+// VISION CUBE SOFTWARE CO. 
+// Middleware: User Role
+// The code that would autheticate the user's roles.
+// This ensures safe and proper access of the users in the system.
+// Contributor/s: 
+// Calulut, Joshua Miguel C.
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class UserRoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $role): Response
+    {
+        if (Auth::check() && Auth::user()->role === $role){
+            return $next($request);
+        }
+
+        return response()->json(['error' => 'Forbidden'], 403);
+    }
+}
