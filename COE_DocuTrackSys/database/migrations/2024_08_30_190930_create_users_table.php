8e35cc8c1633e00aea03421233c3912d5f55f186
Takio Takkaido <papajoshua100804@gmail.com>
2024-08-31 03:52:59 +0800

    Migration for user table

---
+++ COE_DocuTrackSys/database/migrations/2024_08_30_190930_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+// VISION CUBE SOFTWARE CO. 
+// Migration: User
+// Creates the table for the users of the document tracking system
+// Contributor/s: 
+// Calulut, Joshua Miguel C.
+
+use App\Role;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('role')->default(Role::USER->value);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
