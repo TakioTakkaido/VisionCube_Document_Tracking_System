22d1f95af4b925b8a5f687d004f6d59e32c37508
Takio Takkaido <papajoshua100804@gmail.com>
2024-08-31 18:37:18 +0800

    Added document type

---
+++ COE_DocuTrackSys/app/DocumentType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+// VISION CUBE SOFTWARE CO. 
+// Enum: DocumentType
+// Contains the different types of document in the system.
+// Contributor/s: 
+// Calulut, Joshua Miguel C.
+
+enum DocumentType : string
+{
+    case MEMO = 'memo';
+    case LETTER = 'letter';
+    case DEFAULT = 'default';
+}
