acf9d799ae68a61b4c6f34eec437b749e1ccbe32
Takio Takkaido <papajoshua100804@gmail.com>
2024-10-23 20:45:30 +0800

    Document type can now be edited, deleted, added and searched, both changes are reflected in UI and server.

--- COE_DocuTrackSys/resources/js/notification.js
+++ COE_DocuTrackSys/resources/js/notification.js
@@ -3,6 +3,11 @@ $('.notification .close').on('click', function(event) {
     $('#modalAlertContainer').addClass('d-none').removeClass('show'); // Hide the alert
 });
 
+$('.notification').on('click', '.reload', function(event){
+    event.preventDefault();
+    window.location.reload();
+})
+
 export function showNotification(message){
     $('.notification .close').trigger('click');
     $('#modalAlertContainer').removeClass('d-none').addClass('show');
